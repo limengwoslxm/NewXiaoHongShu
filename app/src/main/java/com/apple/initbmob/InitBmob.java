0e1bed51170f53e7cae4e4fe26654ec3ef826a2f
Xiong <xiong_czzn@hotmail.com>
2017-07-22 12:09:44 +0800

    xiong01

--- app/src/main/java/com/apple/initbmob/InitBmob.java
+++ app/src/main/java/com/apple/initbmob/InitBmob.java
@@ -2,15 +2,17 @@ package com.apple.initbmob;
 
 import android.app.Application;
 
+import cn.bmob.v3.Bmob;
+
 
 /**
  * Created by limeng on 2017/7/21.
  */
 
-//public class InitBmob extends Application {
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//        Bmob.initialize(getApplicationContext(),"fee0209c74bf24304798d10e746fd280");
-//    }
-//}
+public class InitBmob extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Bmob.initialize(getApplicationContext(),"fee0209c74bf24304798d10e746fd280");
+    }
+}
